889cf1f9043a48be20e3374bec08ac5d94eef92b
keni-esther <keniesther67@gmail.com>
2026-04-23 13:10:59 +0200

    Clean up blank lines in config.php
    Removed unnecessary blank lines in config.php

--- config.php
+++ config.php
@@ -2,8 +2,6 @@
 // ============================================
 // CVMatch IA - Configuration
 // ============================================
-
-
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
