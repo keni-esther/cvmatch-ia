71fbe53cad875578e23410e95d134f00059855e9
keni-esther <keniesther67@gmail.com>
2026-04-24 21:26:32 +0200

    Activate PHP error reporting in dashboard_recruteur.php
    Enable error reporting for PHP to aid debugging.

--- dashboard_recruteur.php
+++ dashboard_recruteur.php
@@ -1,4 +1,9 @@
 <?php
+// Activer l'affichage de toutes les erreurs PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'config.php';
 if (!estConnecte() || !estRecruteur())
     rediriger('index.php');
